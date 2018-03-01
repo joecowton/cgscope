77f437f1627ba246d2ae3351cab4126ce6714f37
Joe Cowton <joecowton@gmail.com>
2018-03-01 10:45:36 +0000

    passes with input of [1,1]

--- src/median.js
+++ src/median.js
@@ -1,3 +1,13 @@
-function median(n) {}
+function median(n) {
+	const numOfInts = n[0];
+	const ints = n;
+	let median = 0;
+	console.log(numOfInts);
+
+	for (let i = 1; i <= numOfInts; i++) {
+		median += ints[i];
+	}
+	return median;
+}
 
 module.exports = median;
--- test/median.test.js
+++ test/median.test.js
@@ -4,4 +4,8 @@ describe('Median', () => {
 	it('is a function', () => {
 		expect(typeof median).toEqual('function');
 	});
+
+	it('correctly returns output when input is one', () => {
+		expect(median([1, 1])).toEqual(1);
+	});
 });
