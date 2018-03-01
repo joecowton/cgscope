b59f662ef11e39e6974994796b17c1664aedfcdd
Joe Cowton <joecowton@gmail.com>
2018-03-01 13:22:09 +0000

    works for single sentence using cheese

--- mouse_or_mouse/src/mouse_or_mouse.js
+++ mouse_or_mouse/src/mouse_or_mouse.js
@@ -1,3 +1,15 @@
-function mouse_or_mouse(str) {}
+function mouse_or_mouse(str) {
+	const sentenceCount = str[0];
+	const words = [];
+	const string = str.replace(/[~`!@#$%^&*(){}\[\];:"'<,.>?\/\\|_+=-]/g, '');
+
+	for (let word of string.split(' ')) {
+		words.push(word);
+	}
+
+	if (words.includes('cheese')) {
+		return 'animal';
+	}
+}
 
 module.exports = mouse_or_mouse;
--- mouse_or_mouse/test/mouse_or_mouse.test.js
+++ mouse_or_mouse/test/mouse_or_mouse.test.js
@@ -4,4 +4,8 @@ describe('Mouse or Mouse', () => {
 	it('is a function', () => {
 		expect(typeof mouse_or_mouse).toEqual('function');
 	});
+
+	it('returns animal if sentence contains cheese', () => {
+		expect(mouse_or_mouse('2 A mouse usually likes cheese.')).toEqual('animal');
+	});
 });
